591d822ad1b808d2497d42f91892c1329499739a
selena <mie.wenn@live.com>
2023-06-19 02:57:45 +0800

    app-pk update

--- final_project/app_pk/urls.py
+++ final_project/app_pk/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app_pk import views
+from . import views
 
 app_name='app_pk'
 
--- final_project/app_pk/views.py
+++ final_project/app_pk/views.py
@@ -3,28 +3,35 @@ import pandas as pd
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-def load_data_pk():
-    # Read data from csv file
-    df_data_pk = pd.read_csv('app_pk/dataset/pk_cna.csv')
-    
+
+def load_data_pk(cate='產品', topk=3):
+
+    if cate == '公司':
+        df_data_pk = pd.read_csv('app_pk/dataset/pk_3c_corp.csv', sep='|')
+    elif cate == '產品':
+        df_data_pk = pd.read_csv('app_pk/dataset/pk_3c_prod.csv', sep='|')
+
     global data
-    data={}
-    for k,v in zip(df_data_pk.name, df_data_pk.value):
-        data[k]=eval(v)
+    data = {}
+
+    for k, v in zip(df_data_pk.name, df_data_pk.value):
+        data[k] = eval(v)[0:topk]
     
-    # 沒用到的變數刪除之
     del df_data_pk
 
-# load pk data
-load_data_pk()
 
 def home(request):
-    return render(request,'app_pk/home.html')
+    return render(request, 'app_pk/home.html')
 
-# csrf_exempt is used for POST
-# 單獨指定這一支程式忽略csrf驗證
+
 @csrf_exempt
 def api_get_pk_data(request):
+    cate = request.POST.get('cate')
+    topk = request.POST.get('topk')
+    topk = int(topk)
+
+    load_data_pk(cate, topk)
     return JsonResponse(data)
 
-print('Load app pk leaderboard...')
+
+print('app_pk--3C產業聲量大PK--載入成功!')
